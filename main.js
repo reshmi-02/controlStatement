7b4a04fdb9f14ac0c9b3aff13fce7ad0f6a4906c
reshmi-02 <reshmihash123@gmail.com>
2024-12-26 16:53:36 +0530

    add new

---
+++ main.js
@@ -0,0 +1,138 @@
+// // // // // let num ="data";
+
+// // // // // if(num>0){
+// // // // //     console.log(num+" is a positive number");
+// // // // // }
+// // // // // else if(num<0){
+// // // // //     console.log(num+" is a negative number");
+// // // // // }
+// // // // // else if (num==0){
+// // // // //     console.log(num+" is a neutral number");
+// // // // // }
+// // // // // else{
+// // // // //     console.log("Invalid data"); 
+// // // // // }
+
+
+// // // // let num = 23;
+
+// // // // //num -> 2 
+// // // // //       -> 4
+
+// // // // if(num%2==0){
+
+// // // //     console.log(num+" is completely divisible by 2");
+
+// // // //     if(num%4==0){
+// // // //         console.log(num+" is completely divisible by 4");
+// // // //     }
+// // // //     else{
+// // // //         console.log(num+" is not completely  divisible by 4");
+
+// // // //     }
+
+// // // // }
+// // // // else{
+// // // //     console.log(num+" is not completely divisible by 2");
+
+// // // // }
+
+
+// // // // // gender = male , female
+// // // // // age  = 23
+
+// // // // // gender 
+// // // // //     male 
+// // // // //         age>=25  ? eligible : not eligible
+
+// // // // //     female
+// // // // //         age>=21 ? eligible : not eligible
+
+
+// // // let day = 29;
+
+// // // switch (day){
+// // //     case 1:
+// // //         console.log("sunday");
+// // //         break;
+// // //     case 2:
+// // //         console.log("Monday");
+// // //         break;
+// // //     case 3:
+// // //         console.log("Tuesday");
+// // //         break;
+// // //     case 4:
+// // //         console.log("Wednesday");
+// // //         break;
+// // //     case 5:
+// // //         console.log("Thurday");
+// // //         break;
+// // //     case 6:
+// // //         console.log("Friday");
+// // //         break;
+// // //     case 7:
+// // //         console.log("Saturday");
+// // //         break;
+// // //     default:
+// // //         console.log("Invalid data");
+           
+// // // }
+
+
+// // // // operator  = "+","-","*","/","%"
+// // // // num=10,num2=30
+
+
+
+// // let num=1;
+
+// // while(num<=5){  
+// //     console.log(num);
+// //     num++
+// // }
+
+
+// // // 1st iteration 
+
+// // // initialize 
+// // // condition 
+// // // statement
+// // // inc/dec
+
+// // // 2 to 5 
+// // // ------
+// // // condition 
+// // // statement
+// // // inc/dec
+
+
+// let num=1;
+
+// do{
+//     console.log(num);  //1
+//     num++  //2
+// }
+// while(num==4)  //2==4
+
+// 5
+// 4
+// 3
+// 2
+// 1
+
+let num=0;
+for(let i=1;i<=5;i++){  //1<=5 
+
+   if(i==3){
+    continue
+   }
+   console.log(i);
+   
+}
+
+
+// 1 to 10 -> even number 
+// sum of n naatural numbers 
+//n=5  (1 to 5 )
+
+//1 2 3 4 5
